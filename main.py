3ecfd49f73066cfdc6ab41f3ade35e96ba4d06ca
Anderson Souza <anderson.nunes.souza@hotmail.com>
2024-07-04 16:49:46 -0300

    - Improving the script
    Added
    + Added import json on line 2 to use on Last Exibihit
    + Added readfile function to read the file on lines 17 to 20
    + Added json tratative to future improving the Results Exibihit on lines
      104 to 105
    + Altered name of the "game" to 'data' variable name to Future improving using
      the information as a JSON on lines 104 to 108
    
    Removed
    - Removed name of the variable "game" variable name to Future improving using
      the information as a JSON on lines 104 to 108

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 games = []
 current_game = None
@@ -11,96 +12,103 @@ assets_path = ('./assets/')
 file_name = ('qgames.log')
 
 file_path = (f'{assets_path}' + f'{file_name}')
-
-try:
-    #Reads the assets folder, looking for a log file and saves it to a list
+    
+#Reads the assets folder, looking for a log file and saves it to a list
+def readfile():
     with open(file_path, 'r', encoding='cp1252') as file:
         content = file.readlines()
+    return content
 
-        #Splits the list information
-        for line in content:
-            line = line.split()
+try:
+    content = readfile()
+    #Splits the list information
+    for line in content:
+        line = line.split()
 
-            if not line:
-                continue
-             
-             #identifies Start game
-            if "InitGame:" in line:
-                if current_game is not None:
-                    #Adds the line to the games list
-                    games.append(current_game)
-                current_game = {
-                    'players':[{}],
-                    'kills_total':0
-                }
+        if not line:
+            continue
             
-            #Identifies the end of a Game
-            elif "ShutdownGame:" in line:
-                if current_game is not None:
-                        games.append(current_game)
-                        current_game=None
+            #identifies Start game
+        if "InitGame:" in line:
+            if current_game is not None:
+                #Adds the line to the games list
+                games.append(current_game)
+            current_game = {
+                'players':[{}],
+                'kills_total':0
+            }
+        
+        #Identifies the end of a Game
+        elif "ShutdownGame:" in line:
+            if current_game is not None:
+                    games.append(current_game)
+                    current_game=None
 
-            #Collects players info
-            elif "ClientUserinfoChanged:" in line:
-                player_id = line[2]
-                regex = 'r"n\\(.+?)\\t"'
-                new_line = ' '.join(line[3:])
+        #Collects players info
+        elif "ClientUserinfoChanged:" in line:
+            player_id = line[2]
+            regex = 'r"n\\(.+?)\\t"'
+            new_line = ' '.join(line[3:])
 
-                #Regex to adjust Player's Names with spaces
-                player_name = re.search(r'n\\(.*?)\\t', new_line)
-                player_name = player_name.group(1)
-                player = {
-                    "player_id": player_id,
-                    "name": player_name,
-                    "kills": 0
-                }
-                existPlayer = False
-                for playerStored in current_game['players']:
-                    if playerStored == player:
-                        existPlayer = True
-                if not existPlayer:
-                    current_game["players"].append(player)
-                
-                
+            #Regex to adjust Player's Names with spaces
+            player_name = re.search(r'n\\(.*?)\\t', new_line)
+            player_name = player_name.group(1)
+            player = {
+                "player_id": player_id,
+                "name": player_name,
+                "kills": 0
+            }
+            existPlayer = False
+            for playerStored in current_game['players']:
+                if playerStored == player:
+                    existPlayer = True
+            if not existPlayer:
+                current_game["players"].append(player)
+            
+            
 
-            #Collects the kill Information
-            elif "killed" in line:
-                if current_game is not None:
-                    parts = line
+        #Collects the kill Information
+        elif "killed" in line:
+            if current_game is not None:
+                parts = line
 
-                    #join to adjust the Player's Names with spaces
-                    killer_name = ' '.join(parts[5:parts.index('killed')])
-                    killed_index = parts.index('killed') +1
-                    by_index = parts.index('by')
-                    victim_name = ' '.join(parts[killed_index:by_index])
+                #join to adjust the Player's Names with spaces
+                killer_name = ' '.join(parts[5:parts.index('killed')])
+                killed_index = parts.index('killed') +1
+                by_index = parts.index('by')
+                victim_name = ' '.join(parts[killed_index:by_index])
 
-                # Verifies if the killer name is different from the victim name
-                # and add +1 to the game kill count
-                if killer_name != victim_name:
-                    current_game["kills_total"] += 1
+            # Verifies if the killer name is different from the victim name
+            # and add +1 to the game kill count
+            if killer_name != victim_name:
+                current_game["kills_total"] += 1
 
-                    #Runs the current game itens list
-                    for player in current_game['players']:
+                #Runs the current game itens list
+                for player in current_game['players']:
 
-                        #Verifies if the name is present in the current game lists of names
-                        #and if its the same, adds +1 to the player killcount
-                        if isinstance(player, dict):
-                            if 'name' in player:
-                                player_index = current_game['players'].index(player)                                
-                                if killer_name == player['name']:
-                                    current_game["players"][player_index]["kills"] += 1
-                                elif killer_name == '<world>':
-                                    #if current_game["players"][player_index]["kills"] >0:
-                                        current_game["players"][player_index]["kills"] -= 1
-
-
-           
+                    #Verifies if the name is present in the current game lists of names
+                    #and if its the same, adds +1 to the player killcount
+                    if isinstance(player, dict):
+                        if 'name' in player:
+                            player_index = current_game['players'].index(player)                                
+                            if killer_name == player['name']:
+                                current_game["players"][player_index]["kills"] += 1
+                            elif killer_name == '<world>':
+                                    
+                                #(optional)logic that blocks the kills not to substract less than 0
+                                #if current_game["players"][player_index]["kills"] >0:
+                                    current_game["players"][player_index]["kills"] -= 1
+    #Exibihit the end Result     
     x=0
     for game in games:
+        data = json.dumps(game, indent=3)
+        data = json.loads(data)
         print(f'Game {x+1}')
-        print(f"  Total Kills: {game['kills_total']}")
+        print(f"  Total Kills: {data['kills_total']}")
         print(f'Players: '+'{')
-        for player in game['players']:
+
+        #Exibihit the Player's names and Killlist
+        for player in data['players']:
             if isinstance(player, dict):
                 if 'name' in player:
                     print(f'    {player['name']} - Kills: {player['kills']}')
